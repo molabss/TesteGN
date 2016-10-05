f50ba6a5c2d358fc94493c3a834501ea09e76bdf
molabss@gmail.com <molabss@gmail.com>
2016-10-05 12:46:19 -0300

    Adiconado ícones, adicionada tabs, adicionado fragments para offers e leads

--- app/src/main/java/br/com/testmaster/view/MainActivity.java
+++ app/src/main/java/br/com/testmaster/view/MainActivity.java
@@ -1,49 +1,85 @@
 package br.com.testmaster.view;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import br.com.testmaster.R;
+import br.com.testmaster.view.fragments.Leads;
+import br.com.testmaster.view.fragments.Offers;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Pedidos");
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        //drawer.setDrawerListener(toggle);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        //Para evitar o uso de ViewPager
+        //---------------------------------------------------------------------------------------------
+        TextView tabOffer = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOffer.setText("Disponíveis");
+        tabOffer.setCompoundDrawablesWithIntrinsicBounds(R.mipmap.ic_basket, 0, 0, 0);
+
+        TextView tabLead = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabLead.setText("Aceitos");
+        tabLead.setCompoundDrawablesWithIntrinsicBounds(R.mipmap.ic_check, 0 , 0, 0);
+
+        //create tabs title
+        tabLayout.addTab(tabLayout.newTab().setCustomView(tabOffer));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(tabLead));
+        //replace default fragment
+        replaceFragment(new Offers());
+        //handling tab click event
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    replaceFragment(new Offers());
+                } else if (tab.getPosition() == 1) {
+                    replaceFragment(new Leads());
+                }
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content_main, fragment);
+        transaction.commit();
+    }
+//-------------------------------------------------------------------------
+
+
 
     @Override
     public void onBackPressed() {
@@ -83,9 +119,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.mipmap.ic_basket) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.mipmap.ic_check) {
 
         }
 
---
+++ app/src/main/java/br/com/testmaster/view/fragments/Leads.java
@@ -0,0 +1,45 @@
+package br.com.testmaster.view.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.testmaster.R;
+
+
+public class Leads extends Fragment {
+
+    public Leads() {
+
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_leads, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
